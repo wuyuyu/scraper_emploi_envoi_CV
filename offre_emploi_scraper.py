730c4dd65f8d725567de8d886fa05eeefb08788c
Tania <tania.rojas@edu.itescia.fr>
2020-04-29 11:02:39 +0200

    transform js

--- offre_emploi_scraper.py
+++ offre_emploi_scraper.py
@@ -1,24 +1,29 @@
 """
 
-Création d’Un script offre_emploi_scraper.py de scraper:
-Le script prend en paramètre:
-    - Le mot clé qui doit être contenu dans le titre de l’offre
-    - La ville ou le département 
-    - LE TEMPS d’intervalle en heure
-Le script retourne les résultats suivants:
-    - Le titre de offre
-    - Infos de Ville complet 
-    - La page de détails d’offre( Le mail de contact à voir)
-    - Url du offre
-    - La date de publication
-    - l’entreprise 
+ Création d’Un script offre_emploi_scraper.py de scraper:
+ Le script prend en paramètre:
+     - Le mot clé qui doit être contenu dans le titre de l’offre
+     - La ville ou le département
+     - LE TEMPS d’intervalle en heure
+ Le script retourne les résultats suivants:
+     - Le titre de offre
+     - Infos de Ville complet
+     - La page de détails d’offre( Le mail de contact à voir)
+     - Url du offre
+     - La date de publication
+     - l’entreprise
 Le script va stocker les résultats dans un fichier de log
 
-"""
+ """
 
-# import libraries
+#import libraries
+import json
 from bs4 import BeautifulSoup
 import urllib.request
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
 import csv
 
 #mot_cle=input("Entrez le mot clé qui doit être contenu dans le titre de l’offre: ")
@@ -27,18 +32,29 @@ import csv
 #temps_intervalle=input("Entrez le temps d'intervalle en minutes: ")
 
 # specify the url
-urlpage = 'https://www.apec.fr/candidat/recherche-emploi.html/emploi?lieux=711&motsCles=d%C3%A9veloppeur&page=1'
+urlpage = 'https://www.apec.fr/candidat/recherche-emploi.html/emploi?lieux=711&motsCles=d%C3%A9veloppeur&page=0'
+
+options = Options()
+options.headless = True
+
+# run firefox webdriver from executable path of your choice
+driver = webdriver.Chrome(options=options,executable_path=ChromeDriverManager().install())
+driver.get(urlpage)
+
+content = driver.page_source
 
 # query the website and return the html to the variable 'page'
-page = urllib.request.urlopen(urlpage).read()
+#page = urllib.request.urlopen(urlpage)
+print(content)
 # parse the html using beautiful soup and store in variable 'soup'
-soup = BeautifulSoup(page)
+soup = BeautifulSoup(content,'html.parser')
+#jpage = json.load(page)
 
-print(soup)
+#print(jpage)
 
 # find results avec les mots clé
-container = soup.find('div', attrs={'class':'container-result'})
-results = soup.find_all('div',attrs={'class':'card-body'})
+#container = soup.find_next_siblings('div', attrs={'class':'container-result'})
+#results = soup.find_all('div',attrs={'class':'card-body'})
 
 #print(container)
 
